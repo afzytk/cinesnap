b029e59384ec068d230caf13b073391fc407a127
afzytk <afzytk@gmail.com>
2025-11-19 13:57:27 +0530

    feat: add routing for home, movies list, movie details, seat layout, bookings, and favorites

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import Navbar from './components/NavBar'
+import {Route, Routes, useLocation} from "react-router-dom"
+import Home from "./pages/Home"
+import Movies from "./pages/Movies"
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
+ 
 const App = () => {
+
   return (
-    <div>App</div>
+    <>
+       <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/movies" element={<Movies/>} />
+        <Route path="/movies/:id" element={<MovieDetails/>} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings/>} />
+        <Route path="/favorite" element={<Favorite/>} />
+       </Routes>
+    </>
   )
 }
 
